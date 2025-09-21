061704b873f8e200b09c5903950412a84342120b
rutujamusale <rutujamusale846@gmai.com>
2025-09-22 03:52:09 +0530

    Initial Commit

---
+++ src/constants.js
@@ -0,0 +1,248 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+// import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+// import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+// import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+// import gsapLogo from './assets/tech_logo/gsap.png';
+// import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import dotnetLogo from './assets/tech_logo/dotnet.png';
+// import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+// import dsa from './assets/tech_logo/dsa.png';
+import javaLogo from './assets/tech_logo/java.png';
+import sqlLogo from './assets/tech_logo/sql.png';
+// import pythonLogo from './assets/tech_logo/python.png';
+// import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import swagger from './assets/tech_logo/swagger.png';
+import visualStudio from './assets/tech_logo/visualstudio.png';
+import dockerLogo from './assets/tech_logo/docker.png';
+// import mcLogo from './assets/tech_logo/mc.png';
+// import figmaLogo from './assets/tech_logo/figma.png';
+// import netlifyLogo from './assets/tech_logo/netlify.png';
+// import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+// import webverseLogo from './assets/company_logo/webverse_logo.png';
+
+// Education Section Logo's
+import cdacLogo from './assets/education_logo/cdac_logo.jpg';
+import i2itLogo from './assets/education_logo/i2it_logo.png';
+import kthmLogo from './assets/education_logo/kthm_logo.jpg';
+import rachanaLogo from './assets/education_logo/rachana_logo.png';
+
+// Project Section Logo's
+
+import busbookingLogo from './assets/work_logo/bus_book.png';
+import foodondeskLogo from './assets/work_logo/food.png';
+import hotelmgmtLogo from './assets/work_logo/hotel.png';
+import logicclassLogo from './assets/work_logo/logic.jpg';
+import digital from './assets/work_logo/digital.jpg';
+import organicFood from './assets/work_logo/organic.jpg';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: '.NET', logo: dotnetLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      // { name: 'DSA', logo: dsa },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'SQL', logo: sqlLogo },
+
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Swagger', logo: swagger },
+      { name: 'Visual Studio', logo: visualStudio },
+      // { name: 'Docker', logo: dockerLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      // { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: cdacLogo,
+    school: "CDAC, Sunbeam, Pune",
+    date: "Feb 2025 - Aug 2025",
+    grade: "62.25%",
+    desc: "I have completed my Post Graduate Diploma (PG) in Advanced Computing from CDAC, Sunbeam, Pune. During my time at Sunbeam, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at CDAC has been instrumental in shaping my technical abilities and professional growth.",
+    degree: "Diploma in Advanced Computing (PG-DAC)",
+  },
+  {
+    id: 1,
+    img: i2itLogo,
+    school: "International Institute Of Information Technology, Pune",
+    date: "Aug 2019 - May 2023",
+    grade: "8.06 CGPA",
+    desc: "I completed my Bachelor's degree in Information Technology (B.E.) from I2IT College, Pune. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at I2IT College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Bachelor of Engineering - BE (Information Technology)",
+  },
+  {
+    id: 2,
+    img: kthmLogo,
+    school: "KTHM College, Nashik",
+    date: "Apr 2018 - Feb 2019",
+    grade: "57.85%",
+    desc: "I completed my class 12 education from KTHM College, Nashik, under the HSC board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    degree: "Class (XII) - PCM with Computer Science",
+  },
+  {
+    id: 3,
+    img: rachanaLogo,
+    school: "Rachana Vidyalaya, Nashik",
+    date: "May 2016 - March 2017",
+    grade: "86%",
+    desc: "I completed my class 10 education from Rachana Vidyalaya, Nashik, under the SSC board.",
+    degree: "Class(X) - SSC",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Online Bus Ticket Booking System",
+    description:
+      " Implemented core booking features including user registration/login, route and bus search functionality, seat selection interface, and online payment processing. Ensured secure and seamless user experience through authentication integration, responsive UI design, and modular development architecture following industry best practices.",
+    image: busbookingLogo,
+    tags: ["MySQL", "J2EE", "MVC", "React JS", "API"],
+    github: "https://github.com/rutujamusale/DAC-Project-Online-Bus-Ticket-Booking-System",
+
+  },
+  {
+    id: 1,
+    title: "Food on Desk",
+    description:
+      " Developed a QR code-based restaurant ordering web application that allows customers to scan table QR codes, browse menus, and place orders directly without waiter assistance. Built a real-time order management system with instant order transmission to the manager’s dashboard, enabling kitchen staff to receive and process orders immediately for faster service.",
+    image: foodondeskLogo,
+    tags: ["HTML", "CSS", "JavaScript", "MySQL", "Spring boot"],
+    github: "https://github.com/rutujamusale/Food-on-Desk",
+  },
+  {
+    id: 2,
+    title: "Hotel Management System",
+    description:
+      "The Hotel Management System is a Java application that streamlines hotel operations through an interactive GUI. It manages bookings, check-ins, check-outs, room allocation, customer details, and billing in one place. This eliminates manual record keeping and simplifies daily tasks. Admins can also approve or deny booking requests with ease.",
+    image: hotelmgmtLogo,
+    tags: ["Java", "MySQL", "Jframe", "netbeansIDE"],
+    github: "https://github.com/codingmastr/Movie-Recommendation-App",
+  },
+  {
+    id: 3,
+    title: "Logic class Marketing Website",
+    description:
+      "This project's main goal was to create a landing page for promoting the class.",
+    image: logicclassLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    github: "https://github.com/rutujamusale/logic-classes-marketing-website",
+  },
+  {
+    id: 4,
+    title: "Digital Agency Website",
+    description:
+      "This project focuses on developing a fully-fledged and completely responsive front-end landing page for a digital agency. The website works completely fine on all screen sizes and was mainly created keeping the Desktop first look in mind.",
+    image: digital,
+    tags: ["HTML", "CSS", "JavaScript"],
+    github: "https://github.com/rutujamusale/digital-agency-website",
+  },
+  {
+    id: 5,
+    title: "Organic Food Website",
+    description:
+      "This project's main goal was to create a landing page for an E-Commerce Food Website. It employs HTML, CSS, Js, and Bootstrap. This project assisted me in comprehending and expanding my knowledge of BootStrap 5.",
+    image: organicFood,
+    tags: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    github: "https://github.com/rutujamusale/organic-food-website",
+  },
+  // {
+  //   id: 6,
+  //   title: "Coding Master",
+  //   description:
+  //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+  //   image: cmLogo,
+  //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+  //   github: "https://codingmasterweb.in/",
+  //   webapp: "https://codingmasterweb.in/",
+  // },
+  // {
+  //   id: 7,
+  //   title: "Image Search App",
+  //   description:
+  //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+  //   image: imagesearchLogo,
+  //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+  //   github: "https://github.com/codingmastr/Image-Search-App",
+  //   webapp: "https://imagsearch.netlify.app/",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Image Background Remover",
+  //   description:
+  //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+  //   image: removebgLogo,
+  //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+  //   github: "https://github.com/codingmastr/Image-Background-Remover",
+  //   webapp: "https://removeyourbg.netlify.app/",
+  // },
+];  
